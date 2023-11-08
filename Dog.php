48fd4c8754bff304654102bac05da99d9c40e851
Diablo785 <96918963+Diablo785@users.noreply.github.com>
2023-11-08 14:20:56 +0200

    Add files via upload

---
+++ Dog.php
@@ -0,0 +1,22 @@
+<?php
+
+class Dog{
+    public $name;
+    private $age = 6;
+
+    function __construct($name, $age){
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function birthday(){
+        $this->age++;
+        echo $this->age;
+    }
+
+    static function woof(){
+        echo "woof!";
+    }
+}
+
+?>
